ec54fabf9f1692b54c064792f2901926119fd763
kevincamussi <kevincamussi@gmail.com>
2024-01-18 14:21:41 -0300

    tratado erro com catch e finally

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ $(document).ready(function(){
     const follower = $('#seguidores');
     const following = $('#seguindo');
     const profileLink = $('#profile-link');
-    
+    const button = $('#data-button');
+
+
     fetch('https://api.github.com/users/kevincamussi')
     .then(function(resposta){
         return resposta.json();
     })
     .then(function(json){
-        console.log(json);
         
         $(profileAvatar).attr('src', json.avatar_url);
         $(profileName).text(json.name);  
@@ -22,5 +23,10 @@ $(document).ready(function(){
         $(following).text(json.following);
         $(profileLink).attr('href', json.html_url);
     })
+    .catch(function(erro) {
+        console.log('Ocorreu um erro ao tentar acessar o site, tente novamente mais tarde')
+    })
+    .finally(function(){
+        console.log(' Fim da execução do código ')
+    })
 })
-
