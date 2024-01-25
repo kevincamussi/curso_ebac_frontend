84b5d5be2a3774ded54cdf4794cfef9d98200d1f
kevincamussi <kevincamussi@gmail.com>
2024-01-25 11:51:49 -0300

    commit do exericio

---
+++ exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+const multiplicacao = (a, b) => {
+    return a * b;
+};
+console.log(multiplicacao(2, 5));
+const dizOi = (nome) => {
+    return `Olá ${nome}`;
+};
+console.log(dizOi('Kevin'));
---
+++ exercicio_ts.ts
@@ -0,0 +1,11 @@
+const multiplicacao = (a:number, b:number) :number=> {
+    return a*b;
+}
+
+console.log(multiplicacao(2,5));
+
+const dizOi = (nome:string):string =>{
+    return `Olá ${nome}`;
+}
+
+console.log(dizOi('Kevin'));
